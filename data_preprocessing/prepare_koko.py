a4be58714b2a3399ef66a7b6f56fc99254e88cbc
Anisia Katinskaia <a.katinsky@gmail.com>
2019-10-24 11:41:46 +0200

    Replace unreadable with UNK

--- data_preprocessing/prepare_koko.py
+++ data_preprocessing/prepare_koko.py
@@ -25,7 +25,6 @@ error_table_file = 'errors_source_target.csv'
 error_count_file = 'error_statistics.csv'
 
 
-
 def get_original_split(names):
     """
     :param names: list of filename in the dir
@@ -53,6 +52,11 @@ def split_random(names, val=False):
 
 
 def find_error(line):
+    """
+    :param line: raw line with annotation
+    :return: indexes to be replaced, error correction; found error types and info if the line is broken
+    """
+    global number_of_errors
     to_replace = []
     found_types = dict()
     if '<error type' in line:
@@ -60,8 +64,11 @@ def find_error(line):
         if m:
             for match in m:
                 ind = [match.start(), match.end()]
-                error = match.group('error').strip()
+                # it looks safe to remove unreadable tag from errors
+                error = match.group('error').strip().replace('-unreadable-', '')
+                number_of_errors += 1
                 correct = match.group('correct').strip()
+                if_broken = check_correction(error)
                 error_info = match.group('type').strip()
                 if '//' in error_info:
                     types = [el.strip() for el in error_info.split('//')]
@@ -74,10 +81,15 @@ def find_error(line):
                     else:
                         found_types[t].append([error, correct])
                 to_replace.append([ind, error, correct])
-    return to_replace, found_types
+    return to_replace, found_types, if_broken
 
 
 def replace_in_line(line, to_replace):
+    """
+    :param line: raw line
+    :param to_replace: indexes to replace, error and correction
+    :return: line with error, corrected line
+    """
     er_line = ''
     cor_line = ''
     new_start = 0
@@ -91,26 +103,68 @@ def replace_in_line(line, to_replace):
     if new_start < len(line):
         er_line += line[new_start:]
         cor_line += line[new_start:]
+    # unreadable also appears outside of the annotation field
+    # for now replaced with UNK
+    er_line = er_line.replace('-unreadable-', 'UNK')
+    cor_line = cor_line.replace('-unreadable-', 'UNK')
     return er_line, cor_line
 
 
+def balanced(expression):
+    opening = tuple('({[„')
+    closing = tuple(')}]“')
+    mapping = dict(zip(opening, closing))
+    queue = []
+    for letter in expression:
+        if letter in opening:
+            queue.append(mapping[letter])
+        elif letter in closing:
+
+            if not queue or letter != queue.pop():
+                return False
+        else:
+            continue
+    return not queue
+
+
+def check_correction(correct):
+    global broken_annotations
+    symbols = ["{", "}", "(", ")", "[", "]", "„", "“"]
+    found = [c for c in correct if c in symbols]
+    if len(found) == 1:
+        broken_annotations += 1
+        return True
+
+
 if __name__ == "__main__":
+    number_of_errors = 0
+    broken_annotations = 0
     random_split = False
     line_counter = 0
     all_correct_lines = 0
     error_types = dict()
     error_num = dict()
+    files_with_broken = []
+
+    csv_exist = all([os.path.isfile(error_table_file), os.path.isfile(error_count_file)])
+    if csv_exist:
+        print('csv files with already exist, should I rewrite them? (yes/no)')
+        answer = input()
+    if not csv_exist or answer == 'yes':
+        if answer == 'yes':
+            os.remove(error_table_file)
+            os.remove(error_count_file)
+        error_table = codecs.open(error_table_file, 'a')
+        error_count = codecs.open(error_count_file, 'a')
+        writer = True
+        writer_er_type = csv.writer(error_table)
+        writer_er_count = csv.writer(error_count)
+        writer_er_count.writerow(['error_number', 'error_type', 'count'])
+        writer_er_type.writerow(['error_number', 'error_type', 'source', 'target'])
 
     with zipfile.ZipFile(corpus_file, 'r') as zip_ref:
-       zip_ref.extractall(corpus_directory)
+        zip_ref.extractall(corpus_directory)
     names = [name for name in os.listdir(corpus_directory) if name.endswith(".txt")]
-    error_table = codecs.open(error_table_file, 'a')
-    error_count = codecs.open(error_count_file, 'a')
-    writer_er_type = csv.writer(error_table)
-    writer_er_count = csv.writer(error_count)
-    writer_er_count.writerow(['error_number', 'error_type', 'count'])
-    writer_er_type.writerow(['error_number', 'error_type', 'source', 'target'])
-
 
     if random_split:
         split_names = split_random(names, val=True)  # train, test, valid (which is optional)
@@ -124,6 +178,7 @@ if __name__ == "__main__":
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
+    print('Length of splits:')
     for e in split_names:
         print(len(e))
 
@@ -135,21 +190,22 @@ if __name__ == "__main__":
                 for line in f:
                     line_counter += 1
                     if '<error type' in line:
-                        to_replace, found_types = find_error(line)
-                        if 'unreadable' in [e[1] for e in to_replace]:
-                            continue
+                        to_replace, found_types, if_broken = find_error(line)
+                        if if_broken:
+                            files_with_broken.append(filename)
                         for name in found_types.keys():
                             try:
                                 error_number = name.split()[0]
                                 error_type = name[len(error_number):]
                             except:
-                                print('Missing error type!')
+                                print(f'Missing error type in line: {line}')
                                 error_number = '100'
                                 error_type = 'unknown'
                             error_types[error_type] = error_types.get(error_type, 0) + 1
                             error_num[error_type] = error_number
-                            for value in found_types[name]:
-                                writer_er_type.writerow([error_number, error_type, value[0], value[1]])
+                            if writer:
+                                for value in found_types[name]:
+                                    writer_er_type.writerow([error_number, error_type, value[0], value[1]])
                         #print('BEFORE', line)
                         #print(to_replace)
                         er_line, cor_line = replace_in_line(line, to_replace)
@@ -165,14 +221,17 @@ if __name__ == "__main__":
         outfile_source.close()
         outfile_target.close()
     sorted_err_counts = {k: error_types[k] for k in sorted(error_types, key=error_types.get, reverse=True)}
-    for k, v in sorted_err_counts.items():
-        writer_er_count.writerow([error_num[k], k, v])
+
+    if writer:
+        for k, v in sorted_err_counts.items():
+            writer_er_count.writerow([error_num[k], k, v])
 
     print('# correct lines:', all_correct_lines)
     print('# all lines:', line_counter)
     print('# lines with errors:', line_counter - all_correct_lines)
-
-# random plit
+    print('Number of errors:', number_of_errors)
+    print('Broken annotations:', broken_annotations)
+# random split
 # 1202
 # 150
 # 151
